d4e83dfe2afe86762562b8e8225ff15093ea66d5
rahul <rahulkumar3971405@gmail.com>
2026-08-02 10:13:26 +0530

    Move all 0s to end in the given array

---
+++ Problem2.java
@@ -0,0 +1,30 @@
+/*
+Move 0s to end.
+arr[] = [1,2,0,4,3,0,5,0]
+--> [1,2,4,3,5,0,0,0]
+*/
+
+import java.util.Arrays;
+
+class Problem3 {
+    public static void main(String[] args) {
+        int[] arr =new int[]{1,2,0,4,3,0,5,0};
+        pushZeroToEnd(arr);
+
+    }
+    public static void pushZeroToEnd(int[] arr ){
+        int n = arr.length;
+        int index = 0;
+        for(int i =0; i< n; i++){
+            if (arr[i]!=0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        while (index < n ) {
+            arr[index] = 0;
+            index ++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
